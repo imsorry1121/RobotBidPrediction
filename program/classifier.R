507e2409c6c5e0163c14384181808efe6a7ba405
mnicnc404 <mnicnc404@gmail.com>
2015-06-15 17:20:13 +0800

    tuner

--- program/classifier.R
+++ program/classifier.R
@@ -15,9 +15,15 @@ bid.train$outcome = as.factor(bid.train.ans$outcome)
 #bid.test$outcome = bid.test.ans$outcome
 bid.train$id = bid.test$id = NULL
 f = outcome~.
-
-bid.svm = svm(f, bid.train, type = 'C-classification', probability = T, kernel = 'sigmoid')
-pred = data.frame(attr(predict(bid.svm, bid.test, probability = T), "prob"))
+rf = lapply(1:10, function(j) randomForest(f, bid.train, ntree = 130, mtry = 3))
+model = do.call(combine, rf)
+pred = predict(model, bid.test, type='prob')
 bid.test.ans$prediction = pred[,2]
-bid.test.ans = bid.test.ans[,-c(2,3)]
-write.csv(bid.test.ans, "tempResult.csv", row.names = F)
+write.csv(bid.test.ans[,-c(2,3)], 'tempResult.csv', row.names=F)
+
+#bid.tune = tune(svm, f, data = bid.train, kernel = 'radial', range = list(cost = 10^(-2:2), gamma = 10^(-2:2)))
+#bid.svm = svm(f, bid.train, type = 'C-classification', probability = T, kernel = 'radial', cost = 100, gamma = 0.01)
+#pred = data.frame(attr(predict(bid.svm, bid.test, probability = T), "prob"))
+#bid.test.ans$prediction = pred[,2]
+#bid.test.ans = bid.test.ans[,-c(2,3)]
+#write.csv(bid.test.ans, "tempResult.csv", row.names = F)
