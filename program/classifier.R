c50f78c7ca022a80538af153731bc76751571335
mnicnc404 <mnicnc404@gmail.com>
2015-06-21 22:28:18 +0800

    wrap up

--- program/classifier.R
+++ program/classifier.R
@@ -1,9 +1,4 @@
-require(e1071)
 require(randomForest)
-require(C50)
-require(nnet)
-require(adabag)
-
 set.seed(1)
 bid.train = read.csv("../result/featureTrain2015615_1548.csv")
 bid.test = read.csv("../result/featureTest2015615_1548.csv")
@@ -12,22 +7,9 @@ bid.test.ans = read.csv("../result/test.csv")
 bid.train$outcome = as.factor(bid.train$outcome)
 bid.test$outcome = NULL
 f = outcome~.
-#rf = lapply(1:10, function(j) randomForest(f, bid.train, ntree = 50))
-#model = do.call(combine, rf)
-#v = varImp(model)
-#v = cbind(rownames(v), v)
-#vnames = v[order(-v[,2]),1]
-#topV = vnames[1:80]
-#f = as.formula(paste("outcome", paste(topV, collapse=" + "), sep = " ~ "))
-rf = lapply(1:10, function(j) randomForest(f, bid.train, ntree = 50)
+rf = lapply(1:5, function(j) randomForest(f, bid.train, ntree = 140, mtry = 9))
 model = do.call(combine, rf)
 pred = predict(model, bid.test, type='prob')
 bid.test.ans$prediction = pred[,2]
-write.csv(bid.test.ans[,-c(2,3)], 'tempResult.csv', row.names=F)
+write.csv(bid.test.ans[,-c(2,3)], paste("../result/", format(Sys.time(), "%Y.%m.%d.%H.%M.%S"), ".predict.csv", sep=""), row.names=F)
 
-#bid.tune = tune(svm, f, data = bid.train, kernel = 'radial', range = list(cost = 10^(-2:2), gamma = 10^(-2:2)))
-#bid.svm = svm(f, bid.train, type = 'C-classification', probability = T, kernel = 'radial', cost = 100, gamma = 0.01)
-#pred = data.frame(attr(predict(bid.svm, bid.test, probability = T), "prob"))
-#bid.test.ans$prediction = pred[,2]
-#bid.test.ans = bid.test.ans[,-c(2,3)]
-#write.csv(bid.test.ans, "tempResult.csv", row.names = F)
--- program/tune.R
+++ program/tune.R
@@ -1,8 +1,5 @@
-require(e1071)
 require(randomForest)
-require(C50)
-require(nnet)
-require(pROC)
+require(ROCR)
 set.seed(1)
 
 bid.train = read.csv("../result/featureTrain2015615_1548.csv")
@@ -14,27 +11,32 @@ bid.test$outcome = NULL
 f = outcome~.
 k = 10
 tp = 1/k
-n.grid = seq(140, 160, 10)
-t.grid = seq(7, 11, by = 1)
+n.grid = seq(140, 160, by = 10)
+t.grid = seq(5, 9, by = 1)
+f.grid = seq(5, 9, by = 1)
 adata = bid.train
 bid.k = split(adata, f = rep_len(1:k, nrow(adata)))
 p = data.frame()
 for (n in n.grid)
 {	for (t in t.grid)
-	{	print(c(n, t))
-		pp = data.frame()
-		for (i in 1:k)
-		{	set.seed(1)
-			atrain = do.call(rbind, bid.k[setdiff(1:k, i)])
-			atestx = subset(bid.k[[i]], select = -outcome)
-			atesty = bid.k[[i]]$outcome
-			model = do.call(combine, lapply(1:10, function(j) randomForest(f, atrain, ntree = n, mtry = t)))
-			pred = predict(model, atestx, type='prob')
-			p.auc = auc(atesty, pred[,2])
-			pp = rbind(pp, p.auc)
+	{	for (fo in f.grid)
+		{	print(c(n, t, fo))
+			pp = data.frame()
+			for (i in 1:k)
+			{	set.seed(1)
+				atrain = do.call(rbind, bid.k[setdiff(1:k, i)])
+				atestx = subset(bid.k[[i]], select = -outcome)
+				atesty = bid.k[[i]]$outcome
+				model = do.call(combine, lapply(1:fo, function(j) randomForest(f, atrain, ntree = n, mtry = t)))
+				pred = predict(model, atestx, type='prob')
+				p.auc = auc(atesty, pred[,2])
+				pp = rbind(pp, p.auc)
+			}
+			p = rbind(p, c(n, t, fo, mean(pp[,1])))
 		}
-		p = rbind(p, c(n, t, mean(pp[,1])))
 	}
 }
-names(p) = c("ntree", "mtry", "meanAUC") 
-print(p[p[,3]==max(p[,3]),])
+
+names(p) = c("ntree", "mtry", "forests", "meanAUC") 
+print(p[p[,4]==max(p[,4]),])
+plot(performance(prediction(pred[,2], atesty), 'tpr', 'fpr'))
